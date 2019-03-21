7646a7d25f9e999c54ad5953b3e1a0fb9e2de33a
Roman Vovk <wolf8196@gmail.com>
2019-03-21 15:41:23 +0200

    add aes gcm tests

---
+++ Portunus.Tests/Portunus.Tests.Crypto/AEADAes128GcmTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Portunus.Crypto.AuthenticatedEncryption;
+using Portunus.Crypto.Tests;
+
+namespace Portunus.Tests.Crypto
+{
+    [ExcludeFromCodeCoverage]
+    public class AEADAes128GcmTests : AEADTests
+    {
+        public AEADAes128GcmTests()
+            : base(new Aes128Gcm())
+        {
+        }
+    }
+}
---
+++ Portunus.Tests/Portunus.Tests.Crypto/AEADAes256GcmTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Portunus.Crypto.AuthenticatedEncryption;
+using Portunus.Crypto.Tests;
+
+namespace Portunus.Tests.Crypto
+{
+    [ExcludeFromCodeCoverage]
+    public class AEADAes256GcmTests : AEADTests
+    {
+        public AEADAes256GcmTests()
+            : base(new Aes256Gcm())
+        {
+        }
+    }
+}
